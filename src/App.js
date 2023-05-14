becc78b3c2aa6e9f1d99bbdf3686a51976aed134
Ϳαγ ᾥίῘῘίαm Sαρατ <jaywilliamsapar@gmail.com>
2023-05-14 17:26:31 +0800

    fitgirl img

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
     <>
       <img
         className='w-full h-[440px] object-cover'
-        src='https://images.unsplash.com/photo-1504805572947-34fad45aed93?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2607&q=80'
+        src='https://reviewvpn.com/wp-content/uploads/2021/08/KJE.png'
         alt=''
       />
       <div className='relative flex items-center'>
